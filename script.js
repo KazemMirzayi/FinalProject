9acb22ef3b443d1cc106e615bbc5560aa0102cfe
Replit user <>
2023-06-02 10:29:49 +0000

    save changes

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ document.getElementById("printBtn").addEventListener("click", function() {
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
   var Address = document.getElementById("Address").value;
+  var uniName = document.getElementById("uni").value;
+  var graduationDate = document.getElementById("date").value;
+  var subject = document.getElementById("subject").value;
 
   var resultDiv = document.getElementById("result");
-  resultDiv.innerHTML = "<p><h2>Personal Information</h2></p><p><strong>Name:</strong> " + name + "</p>" + "<p><strong>Email:</strong> " + email + "</p>" + "<p><strong>Address</strong> " + Address + "</p>";
+  resultDiv.innerHTML = "<p><h2>Personal Information<hr></h2></p><p><strong>Name:</strong> " + name + "</p>" + "<p><strong>Email:</strong> " + email + "</p>" + "<p><strong>Address</strong> " + Address + "</p><br>" + "<p><h2>Education<hr></h2></p><p><strong>University Name:</strong>" + uniName + "</p>" + "<p><strong>Graduation Date:</strong> " + graduationDate + "</p>" + "<p><strong>Subject</strong> " + subject + "</p>"
 
   window.print();
 });

630f3f9ad430a8868b768dde99784dc33ba2ce42
Replit user <>
2023-06-05 16:42:58 +0000

    save changes

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+document.getElementById("addForm").addEventListener("click", function(addMore){
+  var formsContainer = document.getElementById("forms-container");
+  var newFormContainer = document.createElement("div");
+  newFormContainer.className = "forms-container";
+  newFormContainer.innerHTML = `
+  <div class="form-container">
+  <h2>Add more Education</h2>
+  <label for="uni">University:</label>
+  <input type="text" id="uni" name="uni" required>
+
+  <label for="graduationDate">Graduation Date:</label>
+  <input type="date" id="date" name="date" required>
+
+  <label for="subject">Subject:</label>
+  <input type="text" id="subject" name="subject" required>
+  </div>`;
+  formsContainer.appendChild(newFormContainer);
+  addMore.preventDefault();
+});
+
+
+
+
 document.getElementById("printButton").addEventListener("click", function(){
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;

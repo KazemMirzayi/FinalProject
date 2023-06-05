aee176bbe04f426e0022037c4503b6aa40568124
Replit user <>
2023-06-05 15:37:49 +0000

    add changes

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
-function addForm() {
-      var formsContainer = document.getElementById('forms-container');
-      var newFormContainer = document.createElement('div');
-      newFormContainer.className = 'form-container';
-      newFormContainer.innerHTML = `
-        <label>University:</label>
-        <input type="text" name="university-name[]" required>
-
-        <label>Graduation Date:</label>
-        <input type="date" name="years-attended[]" required>
-
-        <label>Subject:</label>
-        <input type="text" name="field-of-study[]" required>`;
-      formsContainer.appendChild(newFormContainer);
-    }
-
-  
-document.getElementById("printBtn").addEventListener("click", function() {
+document.getElementById("printButton").addEventListener("click", function(){
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
   var Address = document.getElementById("Address").value;
+
   var uniName = document.getElementById("uni").value;
-  var graduationDate = document.getElementById("date").value;
+  var date = document.getElementById("date").value;
   var subject = document.getElementById("subject").value;
-
+  
   var resultDiv = document.getElementById("result");
-  resultDiv.innerHTML = "<p><h2>Personal Information<hr></h2></p><p><strong>Name:</strong> " + name + "</p>" + "<p><strong>Email:</strong> " + email + "</p>" + "<p><strong>Address</strong> " + Address + "</p><br>" + "<p><h2>Education<hr></h2></p><p><strong>University Name:</strong>" + uniName + "</p>" + "<p><strong>Graduation Date:</strong> " + graduationDate + "</p>" + "<p><strong>Subject</strong> " + subject + "</p>" + addToPrint
-
-  window.print();
-});
+  resultDiv.innerHTML = "<div><h2>Personal Information<hr></h2><p><strong>Name:</strong>" + name + "</p><p><strong>Email:</strong>"+email+"</p><p><strong>Address:</strong>" + Address +"</div><div><h2>Education<hr></h2><p><strong>University Name:</strong>" + uniName + "</p><p><strong>Date of Graduation:</strong>" + date + "</p><p><strong>Field of Study:</strong>" + subject +"</p></div>"
+  print();
+  
+})
